2a233128822b59135da800994379c637f1b17080
Brian Chen <thehbc@uvic.ca>
2016-10-04 00:57:12 -0700

    Fixed build issue caused by #24
    It seems the DialogEventArgs class was missing a field for a reviews string. This adds in said field and should prevent Visual Studio from complaining (at least temporarily).

---
+++ DayTomato/DayTomato.Droid/Assets/CreatePinDialogFragment.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+
+using Android.Support.V4.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Maps.Model;
+using DayTomato.Models;
+
+namespace DayTomato.Droid
+{
+	public class CreatePinDialogFragment : DialogFragment
+	{
+		public event EventHandler<DialogEventArgs> DialogClosed;		// Event handler when user presses create
+		private Button _createPinButton;								// Create pin button
+		private Button _cancelButton;									// Cancel create pin button
+		private ImageView _image;										// TODO: Allow user to take photos
+		private TextView _selectedLocationText;							// Location that reverse geocoding got
+		private EditText _name;											// Name user will put
+		private EditText _description;									// Description user will put
+		private RatingBar _rating;										// Rating user will give
+		private EditText _reviewText;										// Rating user will give
+		private bool _createPin;										// Check if they pressed create or not
+
+		public static CreatePinDialogFragment NewInstance(Bundle bundle)
+		{
+			CreatePinDialogFragment fragment = new CreatePinDialogFragment();
+			fragment.Arguments = bundle;
+			return fragment;
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			View view = inflater.Inflate(Resource.Layout.create_pin_dialog_fragment, container, false);
+			_createPinButton = (Button)view.FindViewById(Resource.Id.create_pin_dialog_create_button);
+			_cancelButton = (Button)view.FindViewById(Resource.Id.create_pin_dialog_cancel_button);
+			_image = (ImageView)view.FindViewById(Resource.Id.create_pin_dialog_image);
+			_selectedLocationText = (TextView)view.FindViewById(Resource.Id.create_pin_dialog_location);
+			_name = (EditText)view.FindViewById(Resource.Id.create_pin_dialog_name);
+			_description = (EditText)view.FindViewById(Resource.Id.create_pin_dialog_text_description);
+			_rating = (RatingBar)view.FindViewById(Resource.Id.create_pin_dialog_rating);
+			_reviewText = (EditText)view.FindViewById(Resource.Id.create_pin_dialog_review);
+
+			this.Dialog.SetCancelable(true);
+			this.Dialog.SetCanceledOnTouchOutside(true);
+
+			SetInstances();
+			SetListeners();
+			return view;
+		}
+
+		public override void OnDismiss(IDialogInterface dialog)
+		{
+			base.OnDismiss(dialog);
+
+			// Store and output data to the parent fragment
+			if (DialogClosed != null && _createPin)
+			{
+				DialogClosed(this, new DialogEventArgs
+				{
+					Name = _name.Text,
+					Description = _description.Text,
+					Rating = _rating.Rating,
+					Location = new LatLng(Arguments.GetDouble("SELECTED_LOCATION_LATITUDE"),
+					                      Arguments.GetDouble("SELECTED_LOCATION_LONGITUDE")),
+                    Review = _reviewText.Text
+				});
+			}
+		}
+
+		private void SetInstances()
+		{
+			// Get the location that the user selected
+			_selectedLocationText.Text = Arguments.GetString("SELECTED_LOCATION", "Unknown Location");
+		}
+
+		private void SetListeners()
+		{
+			_createPinButton.Click += (sender, e) =>
+			{
+				Toast.MakeText(this.Activity, "Created Pin", ToastLength.Short).Show();
+				_createPin = true;
+				Dialog.Dismiss();
+			};
+
+			_cancelButton.Click += (sender, e) =>
+			{
+				_createPin = false;
+				Dialog.Dismiss();
+			};
+				
+		}
+	}
+
+	public class DialogEventArgs
+	{
+		public string Name { get; set; }
+		public LatLng Location { get; set; }
+		public string Description { get; set; }
+		public float Rating { get; set; }
+		public string Review { get; set; }
+	}
+}
