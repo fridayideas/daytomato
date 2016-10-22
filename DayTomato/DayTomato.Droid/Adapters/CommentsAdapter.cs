23eaf37c1b86ee7cf7d8e7fffd3dfb25e72a5ed9
Brian Chen <thehbc@uvic.ca>
2016-10-21 23:33:14 -0700

    Clean up Trips (de)serialization.
    Also modified the CommentsAdapter to function if a trip has no comments.

--- DayTomato/DayTomato.Droid/Adapters/CommentsAdapter.cs
+++ DayTomato/DayTomato.Droid/Adapters/CommentsAdapter.cs
@@ -11,14 +11,13 @@ namespace DayTomato.Droid
 {
 	public class CommentsAdapter : BaseAdapter<Comment>
 	{
-		private List<Comment> _comments;
-
-		private Activity _context;
+		private readonly List<Comment> _comments;
+		private readonly Activity _context;
 
 		public CommentsAdapter(Activity context, List<Comment> comments) : base()
 		{
 			_context = context;
-			_comments = comments;
+			_comments = comments ?? new List<Comment>();
 		}
 
 		public override long GetItemId(int position)
@@ -26,20 +25,11 @@ namespace DayTomato.Droid
 			return position;
 		}
 
-		public override int Count
-		{
-			get { return _comments.Count; }
-		}
+		public override int Count => _comments.Count;
 
-		public override Comment this[int position]
-		{
-			get
-			{
-				return _comments[position];
-			}
-		}
+	    public override Comment this[int position] => _comments[position];
 
-		public override View GetView(int position, View convertView, ViewGroup parent)
+	    public override View GetView(int position, View convertView, ViewGroup parent)
 		{
 			View view = convertView; // re-use an existing view, if one is available
 			if (view == null) // otherwise create a new one
--- DayTomato/DayTomato/Models/Trip.cs
+++ DayTomato/DayTomato/Models/Trip.cs
@@ -1,127 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace DayTomato.Models
 {
-	[JsonConverter(typeof(TripConverter))]
     public class Trip
     {
+        [JsonProperty("_id")]
 		public string Id { get; set; }
+        [JsonProperty("pins")]
         public List<Pin> Pins { get; set; }
+        [JsonProperty("name")]
 		public string Name { get; set; }
+        [JsonProperty("type")]
 		public string Type { get; set; }
+        [JsonProperty("linkedAccount")]
 		public string LinkedAccount { get; set; }
+        [JsonProperty("createDate")]
 		public DateTime CreateDate { get; set; }
+        [JsonProperty("likes", DefaultValueHandling = DefaultValueHandling.Populate)]
 		public int Likes { get; set; }
+        [JsonProperty("description")]
 		public string Description { get; set; }
+        [JsonProperty("cost", DefaultValueHandling = DefaultValueHandling.Populate)]
 		public double Cost { get; set; }
+        [JsonProperty("rating", DefaultValueHandling = DefaultValueHandling.Populate)]
 		public float Rating { get; set; }
+        [JsonProperty("comments", TypeNameHandling = TypeNameHandling.Auto)]
 		public List<Comment> Comments { get; set; }
     }
-
-	public class TripConverter : JsonConverter
-	{
-		public override bool CanConvert(Type objectType)
-		{
-			return (objectType == typeof(Trip));
-		}
-
-		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-		{
-			JObject jo = JObject.Load(reader);
-			Trip trip = new Trip();
-			try
-			{
-				trip.Id = (string)jo["_id"];                                // Id of trip
-				trip.Name = (string)jo["name"];                             // Type of trip
-				trip.Type = (string)jo["type"];                             // Name of trip
-				trip.LinkedAccount = (string)jo["linkedAccount"];           // Rating of trip
-				trip.CreateDate = (DateTime)jo["createDate"];               // Description of trip
-				trip.Rating = (float)jo["rating"];                          // Rating of trip
-				trip.Description = (string)jo["description"];               // Description of trip
-				trip.Likes = (int)jo["likes"];                              // trip likes
-				trip.Cost = (double)jo["cost"];                             // trip cost
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex.Message);
-				trip.Rating = 0.0F;
-				trip.Description = "Description";
-				trip.Likes = 0;
-				trip.Cost = 0.00;
-			}
-
-			/* Comments
-             * Comments are in this format:
-             * [{"linkedAccount":111,"text":"LOOOOL","createDate":"2016-09-30T02:44:20.637Z"}]
-             */
-			try
-			{
-				trip.Comments = new List<Comment>();
-				JArray ja = (JArray)jo["comments"];
-				for (int i = 0; i < ja.Count; ++i)
-				{
-					string account = (string)ja[i]["linkedAccount"];
-					string text = (string)ja[i]["text"];
-					DateTime date = (DateTime)ja[i]["createDate"];
-					trip.Comments.Add(new Comment(account, text, date));
-				}
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex.Message);
-			}
-
-			try
-			{
-				trip.Pins = new List<Pin>();
-				JArray ja = (JArray)jo["pins"];
-				for (int i = 0; i < ja.Count; ++i)
-				{
-					Pin pin = JsonConvert.DeserializeObject<Pin>(ja[i].ToString());
-					trip.Pins.Add(pin);
-				}
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex.Message);
-			}
-
-			return trip;
-		}
-
-		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-		{
-			JObject jo = new JObject();
-
-			Trip trip = (Trip)value;
-			jo.Add("_id", trip.Id);
-			jo.Add("type", trip.Type);
-			jo.Add("name", trip.Name);
-			jo.Add("linkedAccount", trip.LinkedAccount);
-			jo.Add("createDate", trip.CreateDate);
-			jo.Add("rating", trip.Rating);
-			jo.Add("description", trip.Description);
-			jo.Add("likes", trip.Likes);
-			jo.Add("cost", trip.Cost);
-
-			JArray ja = new JArray();
-			if (trip.Pins != null)
-			{
-				for (int i = 0; i < trip.Pins.Count; ++i)
-				{
-					JObject jr = new JObject();
-					jr = JObject.Parse(JsonConvert.SerializeObject(trip.Pins[i]));
-					ja.Add(jr);
-				}
-			}
-
-			jo.Add("pins", ja);
-
-			jo.WriteTo(writer);
-		}
-	}
 }
