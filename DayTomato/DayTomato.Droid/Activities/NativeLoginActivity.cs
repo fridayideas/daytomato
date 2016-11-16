15bd8be0da56dcb70e26af31ef3c89626f98967a
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2016-11-15 23:20:55 -0800

    Get resized image. Fixes #119

--- DayTomato/DayTomato.Droid/Activities/NativeLoginActivity.cs
+++ DayTomato/DayTomato.Droid/Activities/NativeLoginActivity.cs
@@ -91,7 +91,7 @@ namespace DayTomato.Droid.Activities
 
                 mainActivityIntent.PutExtra("IdToken", userAccount.IdToken);
                 mainActivityIntent.PutExtra("DisplayName", userAccount.DisplayName);
-                mainActivityIntent.PutExtra("PhotoUrl", userAccount.PhotoUrl.ToString());
+                mainActivityIntent.PutExtra("PhotoUrl", userAccount.PhotoUrl.ToString().Replace("s96-c/", ""));
                 mainActivityIntent.PutExtra("Email", userAccount.Email);
 
                 StartActivity(mainActivityIntent);
