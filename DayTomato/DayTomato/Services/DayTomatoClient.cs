e42a0af542da293e51872b04080993cdb4a67080
Brian Chen <thehbc@uvic.ca>
2016-11-15 23:28:09 -0800

    Simplify DayTomatoClient methods

--- DayTomato/DayTomato/Services/DayTomatoClient.cs
+++ DayTomato/DayTomato/Services/DayTomatoClient.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -14,223 +13,170 @@ namespace DayTomato.Services
 {
     public class DayTomatoClient
     {
-        HttpClient httpClient;
+        private readonly HttpClient _httpClient;
 
-        private readonly string BASE_URL = "http://fridayideas.herokuapp.com";
-
-        public DayTomatoClient()
-        {
-            httpClient = new HttpClient();
-            httpClient.MaxResponseContentBufferSize = 256000;
-        }
+        private const string BaseUrl = "http://fridayideas.herokuapp.com/api/";
 
 		public DayTomatoClient(string idToken)
 		{
-		    httpClient = new HttpClient {MaxResponseContentBufferSize = 256000};
-		    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", idToken);
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl),
+                MaxResponseContentBufferSize = 256000
+            };
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", idToken);
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 		}
 
 		// Get All Trips
 		public async Task<List<Trip>> GetTrips()
 		{
-			List<Trip> trips = new List<Trip>();
-			var uri = new Uri(BASE_URL + "/api/trips");
-			var response = await httpClient.GetAsync(uri);
-			if (response.IsSuccessStatusCode)
-			{
-				string content = await response.Content.ReadAsStringAsync();
-				trips = JsonConvert.DeserializeObject<List<Trip>>(content);
-				return trips;
-			}
+			var response = await _httpClient.GetAsync("trips");
+		    response.EnsureSuccessStatusCode();
 
-			return null;
-		}
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Trip>>(content);
+        }
 
         public async Task<string> CreateTrip(CreateTrip trip)
         {
-            var uri = new Uri(BASE_URL + "/api/trips");
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             var content = new StringContent(JsonConvert.SerializeObject(trip), Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync(uri, content);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = new JObject();
-                res = JObject.Parse(await response.Content.ReadAsStringAsync());
-                return (string)res["_id"];
-            }
-            return "";
+            var response = await _httpClient.PostAsync("trips", content);
+            response.EnsureSuccessStatusCode();
+
+            var res = JObject.Parse(await response.Content.ReadAsStringAsync());
+            return (string)res["_id"];
         }
 
 		public async Task<bool> DeleteTrip(string tripId)
 		{
-			var uri = new Uri(BASE_URL + "/api/trips/" + tripId);
-			var response = await httpClient.DeleteAsync(uri);
+			var response = await _httpClient.DeleteAsync("trips/" + tripId);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Like Trip
 		public async Task<bool> LikeTrip(string tripId, Account account)
 		{
-			var uri = new Uri(BASE_URL + "/api/trips/" + tripId + "/votes/" + account.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			string dir = "{\"dir\": 1}";
-			var content = new StringContent(dir, Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var content = new StringContent(JsonConvert.SerializeObject(new { dir = 1 }),
+                Encoding.UTF8, "application/json");
+			var response = await _httpClient.PutAsync($"trips/{tripId}/votes/{account.Id}", content);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Dislike Trip
 		public async Task<bool> DislikeTrip(string tripId, Account account)
 		{
-			var uri = new Uri(BASE_URL + "/api/trips/" + tripId + "/votes/" + account.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			string dir = "{\"dir\": -1}";
 			var content = new StringContent(dir, Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var response = await _httpClient.PutAsync($"trips/{tripId}/votes/{account.Id}", content);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Remove Votes Trip
 		public async Task<bool> RemoveVoteTrip(string tripId, Account account)
 		{
-			var uri = new Uri(BASE_URL + "/api/trips/" + tripId + "/votes/" + account.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			string dir = "{\"dir\": 0}";
 			var content = new StringContent(dir, Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var response = await _httpClient.PutAsync($"trips/{tripId}/votes/{account.Id}", content);
 			return response.IsSuccessStatusCode;
 		}
 
         // Get Pins
         public async Task<List<Pin>> GetPins()
         {
-            List<Pin> pins = new List<Pin>();
-            var uri = new Uri(BASE_URL + "/api/pins");
-            var response = await httpClient.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                pins = JsonConvert.DeserializeObject<List<Pin>>(content);
-                return pins;
-            }
+            var response = await _httpClient.GetAsync("pins");
+            response.EnsureSuccessStatusCode();
 
-            return null;
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Pin>>(content);
         }
 
         // Get a Pin
         public async Task<Pin> GetPin(string id)
         {
-            Pin pin = new Pin();
-            var uri = new Uri(BASE_URL + "/api/pins/" + id);
-            var response = await httpClient.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                pin = JsonConvert.DeserializeObject<Pin>(content);
-                return pin;
-            }
+            var response = await _httpClient.GetAsync("pins/" + id);
+            response.EnsureSuccessStatusCode();
 
-            return null;
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Pin>(content);
         }
 
         // Get Pins In Area
         public async Task<List<Pin>> GetPins(double latTopLeft, double lngTopLeft, double latBotRight, double lngBotRight)
 		{
 			List<Pin> pins = new List<Pin>();
-			var uri = new Uri(BASE_URL + "/api/pins?searcharea=" + Convert.ToString(latTopLeft) + "," 
+			var uri = new Uri(BaseUrl + "pins?searcharea=" + Convert.ToString(latTopLeft) + ","
 			                  									 + Convert.ToString(lngTopLeft) + ","
 			                 									 + Convert.ToString(latBotRight) + ","
 			                 									 + Convert.ToString(lngBotRight));
-			var response = await httpClient.GetAsync(uri);
-			if (response.IsSuccessStatusCode)
-			{
-				string content = await response.Content.ReadAsStringAsync();
-				pins = JsonConvert.DeserializeObject<List<Pin>>(content);
-				return pins;
-			}
+			var response = await _httpClient.GetAsync(uri);
+		    response.EnsureSuccessStatusCode();
 
-			return null;
-		}
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Pin>>(content);
+        }
 
-		// Get hot pins
-		public async Task<List<Pin>> GetHotPins()
+        // Get hot pins
+        public async Task<List<Pin>> GetHotPins()
 		{
-			List<Pin> pins = new List<Pin>();
-			var uri = new Uri(BASE_URL + "/api/pins?sort=likes&limit=10");
-			var response = await httpClient.GetAsync(uri);
-			if (response.IsSuccessStatusCode)
-			{
-				string content = await response.Content.ReadAsStringAsync();
-				pins = JsonConvert.DeserializeObject<List<Pin>>(content);
-				return pins;
-			}
-			return null;
-		}
+			var response = await _httpClient.GetAsync("pins?sort=likes&limit=10");
+		    response.EnsureSuccessStatusCode();
 
-		// Create Pins
-		public async Task<string> CreatePin(Pin pin)
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Pin>>(content);
+        }
+
+        // Create Pins
+        public async Task<string> CreatePin(Pin pin)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins");
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			var content = new StringContent(JsonConvert.SerializeObject(pin), Encoding.UTF8, "application/json");
-			var response = await httpClient.PostAsync(uri, content);
-			if (response.IsSuccessStatusCode)
-			{
-				var res = new JObject();
-				res = JObject.Parse(await response.Content.ReadAsStringAsync());
-				return (string)res["_id"];
-			}
-			return "";
-		}
+			var response = await _httpClient.PostAsync("pins", content);
+		    response.EnsureSuccessStatusCode();
 
-		// Delete Pin
-		public async Task<bool> DeletePin(Pin pin)
+            var res = JObject.Parse(await response.Content.ReadAsStringAsync());
+            return (string)res["_id"];
+        }
+
+        // Delete Pin
+        public async Task<bool> DeletePin(Pin pin)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pin.Id);
-			var response = await httpClient.DeleteAsync(uri);
+			var response = await _httpClient.DeleteAsync("pins/" + pin.Id);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Update Pin
 		public async Task<bool> UpdatePin(Pin pin)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pin.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			var content = new StringContent(JsonConvert.SerializeObject(pin), Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var response = await _httpClient.PutAsync("pins/" + pin.Id, content);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Like Pin
 		public async Task<bool> LikePin(string pinId, Account account)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pinId + "/votes/" + account.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			string dir = "{\"dir\": 1}";
 			var content = new StringContent(dir, Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var response = await _httpClient.PutAsync($"pins/{pinId}/votes/{account.Id}", content);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Dislike Pin
 		public async Task<bool> DislikePin(string pinId, Account account)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pinId + "/votes/" + account.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			string dir = "{\"dir\": -1}";
 			var content = new StringContent(dir, Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var response = await _httpClient.PutAsync($"pins/{pinId}/votes/{account.Id}", content);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Remove Votes Pin
 		public async Task<bool> RemoveVotePin(string pinId, Account account)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pinId + "/votes/" + account.Id);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			string dir = "{\"dir\": 0}";
 			var content = new StringContent(dir, Encoding.UTF8, "application/json");
-			var response = await httpClient.PutAsync(uri, content);
+			var response = await _httpClient.PutAsync($"pins/{pinId}/votes/{account.Id}", content);
 			return response.IsSuccessStatusCode;
 		}
 
@@ -238,13 +184,11 @@ namespace DayTomato.Services
 		// Need to pass {text:<string>, linkedAccount:<string>}
 		public async Task<bool> AddCommentToPin(Pin pin, string text, string linkedAccount)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pin.Id + "/comments");
 			JObject comment = new JObject();
 			comment.Add("text", text);
 			comment.Add("linkedAccount", linkedAccount);
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			var content = new StringContent(JsonConvert.SerializeObject(comment), Encoding.UTF8, "application/json");
-			var response = await httpClient.PostAsync(uri, content);
+			var response = await _httpClient.PostAsync($"pins/{pin.Id}/comments", content);
 			return response.IsSuccessStatusCode;
 		}
 
@@ -252,8 +196,7 @@ namespace DayTomato.Services
 		// Need to pass the pin and account ID
 		public async Task<bool> DeleteCommentFromPin(Pin pin, string accountId)
 		{
-			var uri = new Uri(BASE_URL + "/api/pins/" + pin.Id + "/comments/" + accountId);
-			var response = await httpClient.DeleteAsync(uri);
+			var response = await _httpClient.DeleteAsync($"pins/{pin.Id}/comments/{accountId}");
 			return response.IsSuccessStatusCode;
         }
 
@@ -271,52 +214,46 @@ namespace DayTomato.Services
         /// <returns></returns>
         public async Task<Account> GetAccount()
         {
-            var uri = new Uri(BASE_URL + "/api/accounts/currentuser");
-            var response = await httpClient.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
+            var response = await _httpClient.GetAsync("accounts/currentuser");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var accountParsed = JObject.Parse(content);
+            return new Account
             {
-                string content = await response.Content.ReadAsStringAsync();
-                JObject accountParsed = JObject.Parse(content);
-                Account account = new Account
-                {
-                    Id = (string)accountParsed["_id"],
-                    Username = (string)accountParsed["auth0Id"],//TODO: This is a temporary placeholder for username (should use a real username once /accounts/currentuser route is updated)
-                    Seeds = (double)accountParsed["numSeeds"],
-                    Pins = (int)accountParsed["numPins"]
-                };
-
-                return account;
-            }
-
-            return null;
+                Id = (string)accountParsed["_id"],
+                Username = (string)accountParsed["auth0Id"], // TODO: This is a temporary placeholder for username (should use a real username once /accounts/currentuser route is updated)
+                Seeds = (double)accountParsed["numSeeds"],
+                Pins = (int)accountParsed["numPins"]
+            };
         }
 
-		// Create Account
-		// Need to pass {username:<string>, token:<string>}
-		public async Task<bool> CreateAccount(JObject account)
+        // Create Account
+        // Need to pass {username:<string>, token:<string>}
+        public async Task<bool> CreateAccount(JObject account)
 		{
-			var uri = new Uri(BASE_URL + "/api/accounts");
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			var content = new StringContent(account.ToString(), Encoding.UTF8, "application/json");
-			var response = await httpClient.PostAsync(uri, content);
+			var response = await _httpClient.PostAsync("accounts", content);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Change seed amount of account
 		// Need to pass account ID and amount
+        // TODO do we need this?
 		public async Task<bool> UpdateAccountSeeds(string accountId, double amount)
 		{
-			var uri = new Uri(BASE_URL + "/api/accounts/seeds/" + accountId + "/" + amount);
-			var response = await httpClient.PutAsync(uri, null);
+			var uri = new Uri(BaseUrl + "/api/accounts/seeds/" + accountId + "/" + amount);
+			var response = await _httpClient.PutAsync(uri, null);
 			return response.IsSuccessStatusCode;
 		}
 
 		// Change pin amount of account
 		// Need to pass account ID and amount
+        // TODO do we need this?
 		public async Task<bool> UpdateAccountPins(string accountId, int amount)
 		{
-			var uri = new Uri(BASE_URL + "/api/accounts/pins/" + accountId + "/" + amount);
-			var response = await httpClient.PutAsync(uri, null);
+			var uri = new Uri(BaseUrl + "/api/accounts/pins/" + accountId + "/" + amount);
+			var response = await _httpClient.PutAsync(uri, null);
 			return response.IsSuccessStatusCode;
 		}
 
